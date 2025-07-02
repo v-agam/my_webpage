9c1df14be800191327b33b50b18bb085799d9dec
V.Agam <85820389+v-agam@users.noreply.github.com>
2025-07-02 06:41:15 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -35,7 +35,7 @@ export default function Hero() {
             className="text-xl md:text-2xl mb-8 max-w-3xl mx-auto relative z-20 text-primary-600 dark:text-gray-200"
             style={{ textShadow: '0 2px 8px rgba(0,0,0,0.14)' }}
           >
-            A Critical Thinker, Lead Data Scientist, Researcher &amp Teacher;
+            A Critical Thinker, Lead Data Scientist, Researcher & Teacher
           </p>
           
           <div className="flex flex-col sm:flex-row gap-4 justify-center items-center">
